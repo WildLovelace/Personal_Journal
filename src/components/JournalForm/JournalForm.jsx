6bb52c4f5ccefb19458efeb9ab59cb14bda0af34
Mikhail <tortmort123456789@gmail.com>
2026-01-16 13:49:58 +0300

    com2

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,38 +1,70 @@
-import './JournalForm.css'
+import styles from './JournalForm.module.css'
 import { useState } from 'react'
 import Button from '../Button/Button'
+import cn from 'classnames';
 
 function JournalForm({ onSubmit }) {
-    //STATEFULL КОМПОНЕНТ, ТК ИМЕЕТ ЛОГИКУ И УПРАВЛЯЕТ СОСТОЯНИЕМ
-    // const [inputData, setInputData] = useState('');
-
-    // const inputChange = (e) => {
-    //     setInputData(e.target.value);
-    //     console.log(inputData);
-    // };
-    //<input type="text" name='tag' value={inputData} onChange={inputChange} />
+    const [formValideState, setFormValideState] = useState({ title: true, post: true, date: true });
 
     const addJournalItem = (e) => {
         e.preventDefault();
 
-        //React сравнивает объекты по ссылке (shallow comparison)
-        //state и setState(state) передают одну и ту же ссылку на объект
-        //React видит, что ссылка не изменилась, и пропускает ререндер
-
-        //Всегда создавай новый объект/массив при изменении состояния в React
-
         const formData = new FormData(e.target);
         const formProps = Object.fromEntries(formData);
+
+        let isFormValid = true;
+        if (!formProps.title?.trim().length) {
+            setFormValideState(state => ({ ...state, title: false }));
+            isFormValid = false;
+        } else {
+            setFormValideState(state => ({ ...state, title: true }));
+        }
+
+        if (!formProps.post?.trim().length) {
+            setFormValideState(state => ({ ...state, post: false }));
+            isFormValid = false;
+        } else {
+            setFormValideState(state => ({ ...state, post: true }));
+        }
+
+        if (!formProps.date) {
+            setFormValideState(state => ({ ...state, date: false }));
+            isFormValid = false;
+        } else {
+            setFormValideState(state => ({ ...state, date: true }));
+        }
+
+        if (!isFormValid) {
+            return;
+        }
         onSubmit(formProps);
     }
 
     return (
         <>
-            <form className='journal-form' onSubmit={addJournalItem} >
-                <input type="title" name='title' />
-                <input type="date" name='date' />
-                <input type="text" name='tag' />{/* контролируемый компонент */}
-                <textarea name="text  " cols="30" rows="10"></textarea>
+            <form className={styles['journal-form']} onSubmit={addJournalItem} >
+                <div>
+                    <input type="title" name='title' className={`${styles['input-title']} ${formValideState.title ? '' : styles['invalid']}`} />
+                </div>
+
+                <div className={styles['form-row']}>
+                    <label for="date" className={styles['form-label']}>
+                        <img width={20} height={20} src="./calendar.svg" alt="" />
+                        <span>Дата</span>
+                    </label>
+                    <input type="date" name='date' id='date' className={`${styles['input']} ${formValideState.date ? '' : styles['invalid']}`} />
+                </div>
+
+                <div className={styles['form-row']}>
+                    <label for="tag" className={styles['form-label']}>
+                        <img width={20} height={20} src="./folder.svg" alt="" />
+                        <span>Тэг</span>
+                    </label>
+                    <input type="text" name='tag' id='tag' />
+                </div>
+
+
+                <textarea name="post" cols="30" rows="10" className={`${styles['input']} ${formValideState.post ? '' : styles['invalid']}`} />
                 <Button text="Сохранить" />
             </form>
         </>
